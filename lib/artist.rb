decfcd3811f491eb9c3a9746241a7229333227f4
David Stewart <davidstewart@gmail.com>
2019-04-13 20:36:06 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,9 +3,9 @@ require 'pry'
 class Artist
   attr_accessor :name
   attr_reader :songs
-  extend Memorable
+  extend Memorable::ClassMethods
   extend Findable::ClassMethods
-  include Paramable
+  include Paramable::InstanceMethods
   @@artists = []
 
   def initialize
