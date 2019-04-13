8b3846cf0a839c17d6569fd2e925790f79d50dca
David Stewart <davidstewart@gmail.com>
2019-04-13 20:45:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,11 +6,12 @@ class Song
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
   include Paramable::InstanceMethods
+  include Memorable::InstanceMethods
   @@songs = []
 
-  def initialize
-    self.class.all << self
-  end
+  # def initialize
+  #   self.class.all << self
+  # end
 
   # def self.find_by_name(name)
   #   @@songs.detect{|a| a.name == name}
